8854f838feebbdf177801b58c3f3c9a7ad1ace7d
Pixel8edstudios <50810194+Pixel8edstudios@users.noreply.github.com>
2021-03-20 15:30:15 -0400

    removed uneeded time.sleep()

--- P2P_Drone/drone_base_cogs/scanner_cogs/neighborhood_scanner.py
+++ P2P_Drone/drone_base_cogs/scanner_cogs/neighborhood_scanner.py
@@ -25,8 +25,7 @@ if not os.path.isdir(lock_dir):
 def peer_scan():
     while os.path.exists(lock_file_path):
         #print("scanner_locked")
-        time.sleep(1)
-    time.sleep(random.uniform(0.1,1.0))
+        time.sleep(random.uniform(0.1,1.0))
     try:
         peer_lock = open(lock_file_path, "x")
         peer_lock.close()
